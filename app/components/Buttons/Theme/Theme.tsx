b1d19cff5d87ab3c1545e91e2d13cffe6ce5cee1
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-18 15:00:52 -0600

    Refactor theme
    Import useState and useEffect
    
    Create mounted state and theme
    
    Create useEffect to set mounded to true when component is rendered
    
    Set conditional if component is not rendered return null
    
    Refactor jsx to set theme on button click and conditionaly render icon

--- app/components/Buttons/Theme/Theme.tsx
+++ app/components/Buttons/Theme/Theme.tsx
@@ -1,10 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 import Icon from "../../Icon/Icon";
 
-export default function Theme() {
+export default function ThemeButton() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <button className="bg-slate700 p-4 rounded-lg">
-      {/* Set conditional for icon switch */}
-      <Icon iconVariant="sun" />
+    <button
+      className=" p-4 rounded-lg"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? (
+        <Icon iconVariant="sun" />
+      ) : (
+        <Icon iconVariant="moon" />
+      )}
     </button>
   );
 }
