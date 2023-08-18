dd7a9f4d929784ff5cede0d8648d2e7d1e1dbcf6
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-18 11:00:32 -0600

    Create PageLink component

---
+++ app/components/links/PageLink.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useRef, useEffect } from "react";
+import Link from "next/link";
+
+type PageLinkProps = {
+  text: string;
+  href: string;
+  id: string;
+} & React.ComponentProps<"a">;
+
+export default function PageLink({ text, href, id }: PageLinkProps) {
+  const linkRef = useRef<HTMLAnchorElement | null>(null);
+
+  useEffect(() => {
+    const focusLinkId =
+      window.location.pathname === "/" ? "coinsLink" : "portfolioLink";
+    if (linkRef.current) {
+      if (linkRef.current.id === id && linkRef.current.id === focusLinkId) {
+        linkRef.current.focus();
+      }
+    }
+  }, [id]);
+
+  return (
+    <Link
+      id={id}
+      ref={linkRef}
+      className="focus:bg-slate700 focus:outline-none flex items-center py-2 px-7 rounded-lg"
+      href={href}
+    >
+      {text}
+    </Link>
+  );
+}
