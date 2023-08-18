e350566bbad34d64e257c656dd54266f0c03b4b0
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-18 10:54:09 -0600

    Add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white100: "#ffffff",
+        green200: "#00FC2A",
+        slate700: "#2C2F36",
+        slate800: "#1F2128",
+        slate900: "#191b1f",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
