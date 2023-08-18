98219fe10c00fd6564122d69e07970e6a4610486
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-18 11:05:26 -0600

    Create nav in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import PageLink from "./components/links/PageLink";
+import Icon from "./components/Icon/Icon";
+import FormInput from "./components/Inputs/FormInput";
+import CustomSelect from "./components/Inputs/CustomSelect";
+import Theme from "./components/Theme/Theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="font-medium container flex items-center justify-between mx-auto">
+          <ul className="text-white100 text-1xl flex">
+            <li>
+              <PageLink id="coinsLink" href="/" text="Coins" />
+            </li>
+            <li>
+              <PageLink
+                id="portfolioLink"
+                href="../pages/Portfolio"
+                text="Portfolio"
+              />
+            </li>
+          </ul>
+
+          <form className="text-white100 flex gap-5" action="">
+            <div className="relative flex items-center">
+              <Icon className="text-xl absolute ml-2.5" iconVariant="search" />
+              <FormInput
+                label="Search"
+                id="search"
+                type="text"
+                name="search"
+                placeholder="Search"
+              />
+            </div>
+
+            <CustomSelect />
+            <Theme />
+          </form>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
