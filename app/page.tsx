47bf9608933e3275d8c716c87fc4874376d8ad8c
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-22 15:27:58 -0600

    Refactor Page
    Remove charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
+import ChartsOverview from "./components/ChartsOverview/Charts";
+
 export default function Home() {
   return (
-    <main className="container bg-slate700 mx-auto pt-20 px-24">
-      <h2 className="mb-7">Your overview</h2>
-
-      <div className="flex gap-7 mb-20">
-        <div className="bg-slate900 max-w-3.5xl max-h-md">
-          <p>BTC</p>
-          <p>$13.431 mln</p>
-          <p>Jun 14, 2021</p>
-        </div>
-
-        <div className="bg-slate900 max-w-3.5xl max-h-md">
-          <p>Volume 24hr</p>
-          <p>$807.24 bln</p>
-          <p>Jun 14, 2021</p>
-        </div>
-      </div>
+    <main className="bg-slate700 mx-auto pt-20 px-24 w-screen">
+      <ChartsOverview />
+      {/* Table Here */}
     </main>
   );
 }
