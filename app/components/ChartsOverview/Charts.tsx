6eac17f1b5afd67a7de435a0a4c4fe2cf8011ba3
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-22 15:39:04 -0600

    Test

--- app/components/ChartsOverview/Charts.tsx
+++ app/components/ChartsOverview/Charts.tsx
@@ -15,7 +15,7 @@ export default function ChartsOverview() {
         <div className="bg-slate900 max-w-3.5xl max-h-md">
           <p>Volume 24hr</p>
           <p>$807.24 bln</p>
-          <p>Jun 14, 2021</p>
+          <p>Jun 21, 2021</p>
         </div>
       </div>
     </div>
