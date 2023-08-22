cce8ee0c81c17a2b89271094da70e554b9ac4002
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-22 11:50:11 -0600

    Create home component base jsx layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-    <main className="container mx-auto">
-      <div>Home Page</div>
+    <main className="container bg-slate700 mx-auto pt-20 px-24">
+      <h2 className="mb-7">Your overview</h2>
+
+      <div className="flex gap-7 mb-20">
+        <div className="bg-slate900 max-w-3.5xl max-h-md">
+          <p>BTC</p>
+          <p>$13.431 mln</p>
+          <p>Jun 14, 2021</p>
+        </div>
+
+        <div className="bg-slate900 max-w-3.5xl max-h-md">
+          <p>Volume 24hr</p>
+          <p>$807.24 bln</p>
+          <p>Jun 14, 2021</p>
+        </div>
+      </div>
     </main>
   );
 }
