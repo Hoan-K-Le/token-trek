8f489e43ea1537cc46c1ae507722a8fcf36b46c2
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-23 11:23:01 -0600

    Refactor Home
    Create header and wrap charts overview with a div tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import ChartsOverview from "./components/ChartsOverview/Charts";
 
 export default function Home() {
   return (
-    <main className="bg-slate700 mx-auto pt-20 px-24 w-screen">
-      <ChartsOverview />
+    <main className="bg-slate700 max-w-8xl px-24 pt-20 mx-auto">
+      <header>
+        <h2 className="text-xl mb-7">Your overview</h2>
+      </header>
+
+      <div className="flex gap-7 mb-20">
+        <ChartsOverview />
+      </div>
+
       {/* Table Here */}
     </main>
   );
