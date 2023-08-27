99cb568fc36dfb884bd4753125e5a637559d62dd
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-27 11:40:16 -0600

    Refactor Line Chart

--- app/components/Charts/LineChart.tsx
+++ app/components/Charts/LineChart.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -27,53 +28,82 @@ const options = {
   },
 };
 
-function ChartsOverview({ chartLabels, chartData }) {
+const ChartsOverview = () => {
+  const [bitcoinPrices, setBitcoinPrices] = useState<number[]>([]);
+  const [bitcoinPriceDates, setBitcoinPriceDates] = useState<number[]>([]);
+
+  const bitcoinPriceLabels = bitcoinPriceDates.map((date) =>
+    new Date(date).getDate().toString()
+  );
+
+  const getBitcoinPrices = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=180&interval=daily"
+      );
+
+      const prices = data.prices.map((price: [number, number]) => price[1]);
+      const dates = data.prices.map((price: [number, number]) => price[0]);
+
+      setBitcoinPrices(prices);
+      setBitcoinPriceDates(dates);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBitcoinPrices();
+  }, []);
+
   const currentTheme = localStorage.getItem("theme");
   const borderColor = currentTheme === "dark" ? "#00FC2A" : "#3D63EC";
 
-  chartData = {
-    labels: chartLabels,
+  const setBackgroundColor = () => {
+    if (!canvasRef.current) {
+      return null;
+    }
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) {
+      return null;
+    }
+
+    const chartArea = {
+      top: 0,
+      bottom: canvasRef.current.height,
+    };
+
+    const gradient = ctx.createLinearGradient(
+      0,
+      chartArea.bottom,
+      0,
+      chartArea.top
+    );
+    if (currentTheme === "dark") {
+      gradient.addColorStop(0, "#192021"); // Start color
+      gradient.addColorStop(0.4, "#23322E"); // Mid color
+      gradient.addColorStop(1, "#37413F"); // End color
+    } else if (currentTheme === "light") {
+      gradient.addColorStop(0, "#F9FAFF"); // Start color
+      gradient.addColorStop(0.4, "#ECF0FD"); // Mid color
+      gradient.addColorStop(1, "#D9E1FB"); // End color
+    }
+
+    return gradient;
+  };
+
+  const data = {
+    labels: bitcoinPriceLabels,
     datasets: [
       {
         fill: true,
         label: "Prices",
-        data: chartData,
+        data: bitcoinPrices,
         borderColor: borderColor,
         pointStyle: "circle",
         pointRadius: 0,
         tension: 0.4,
-        backgroundColor: () => {
-          if (!canvasRef.current) {
-            return null;
-          }
-          const ctx = canvasRef.current.getContext("2d");
-          if (!ctx) {
-            return null;
-          }
-
-          const chartArea = {
-            top: 0,
-            bottom: canvasRef.current.height,
-          };
-
-          const gradient = ctx.createLinearGradient(
-            0,
-            chartArea.bottom,
-            0,
-            chartArea.top
-          );
-          if (currentTheme === "dark") {
-            gradient.addColorStop(0, "#192021"); // Start color
-            gradient.addColorStop(0.4, "#23322E"); // Mid color
-            gradient.addColorStop(1, "#37413F"); // End color
-          } else if (currentTheme === "light") {
-            gradient.addColorStop(0, "#F9FAFF"); // Start color
-            gradient.addColorStop(0.4, "#ECF0FD"); // Mid color
-            gradient.addColorStop(1, "#D9E1FB"); // End color
-          }
-
-          return gradient;
-        },
+        backgroundColor: setBackgroundColor(),
       },
     ],
   };
@@ -81,10 +111,10 @@ function ChartsOverview({ chartLabels, chartData }) {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   return (
     <>
-      <Line data={chartData} options={options} />
+      <Line data={data} options={options} />
       <canvas className="hidden" ref={canvasRef} />
     </>
   );
-}
+};
 
 export default ChartsOverview;
