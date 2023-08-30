6e5c2db6c0f5dd2f8f1faa7186d52934d39dcaba
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-08-30 08:54:47 -0600

    Add missing semi colons

--- app/components/TableOverview/CoinPrice/CoinPrice.tsx
+++ app/components/TableOverview/CoinPrice/CoinPrice.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { TableDataProps } from '../TableDataProps/TableDataProps'
-const CoinPrice = ({ coin }: { coin: TableDataProps }) => {
-  return <td className="py-4">${coin.current_price.toLocaleString('en-US')}</td>
-}
+import React from "react";
+import { TableDataProps } from "../TableDataProps/TableDataProps";
 
-export default CoinPrice
+const CoinPrice = ({ coin }: { coin: TableDataProps }) => {
+  return (
+    <td className="py-4">${coin.current_price.toLocaleString("en-US")}</td>
+  );
+};
+
+export default CoinPrice;
